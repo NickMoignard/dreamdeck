6adb91ead8c736ab0a763d09721ca71947382a1d
NickMoignard <nicklmoignard@gmail.com>
2020-11-18 20:27:20 +1100

    initital work

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const pageStyle = {
+    height: "100%",
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+};
+
+const Page = () => {
+    return <div style={pageStyle} className="row"><h1>PAGE</h1></div>;
+}; 
+
+export default Page;
